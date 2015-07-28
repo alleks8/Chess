e47185e37a489f1218c1c60e0c5060173940c3f8
Alexandru Serdean <serdeanalexandru@gmail.com>
2015-07-28 15:51:31 +0300

    Initial commit

---
+++ JS_Test/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace JS_Test.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
